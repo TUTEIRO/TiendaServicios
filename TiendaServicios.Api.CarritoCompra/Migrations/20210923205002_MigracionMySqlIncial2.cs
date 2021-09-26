92e73b390dbdb1aadb10eff94ce2cf021a1af81e
Carlos Quintero <carlos.quintero@norsoft.com.co>
2021-09-26 16:24:09 -0500

    Agregar archivos de proyecto.

---
+++ TiendaServicios.Api.CarritoCompra/Migrations/20210923205002_MigracionMySqlIncial2.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TiendaServicios.Api.CarritoCompra.Migrations
+{
+    public partial class MigracionMySqlIncial2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "CarritoSesionid",
+                table: "CarritoSesion",
+                newName: "CarritoSesionId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "CarritoSesionId",
+                table: "CarritoSesion",
+                newName: "CarritoSesionid");
+        }
+    }
+}
